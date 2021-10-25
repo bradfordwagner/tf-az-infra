b3c2254e0e7ebcbb1123aa63d08a402f8604b07e
Bradford Wagner <bwagner@blackrock.com>
2021-10-24 22:31:29 -0400

    Initialize

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_pet" "rg-name" {
+  prefix = "bw-test"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg-name.id
+  location = var.region
+}
+
+resource "azurerm_user_assigned_identity" "aks" {
+  name                = "aks"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_kubernetes_cluster" "infra" {
+  name                = "infra"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "bradfordwagner"
+  kubernetes_version  = "1.21.2"
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard" # basic made it unable to connect k9s
+  }
+
+  default_node_pool {
+    name                 = "default"
+    vm_size              = "standard_d2a_v4"
+    #   vm_size    = "Standard_D2_v2"
+    enable_auto_scaling  = true
+    min_count            = 1
+    max_count            = 3
+    orchestrator_version = "1.21.2"
+  }
+
+  identity {
+    type                      = "UserAssigned"
+    user_assigned_identity_id = azurerm_user_assigned_identity.aks.id
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
