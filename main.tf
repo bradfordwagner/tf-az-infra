f258d73ab69e5ead05f19db139ba209c3e1fce82
Bradford Wagner <bwagner@blackrock.com>
2022-02-16 21:47:39 -0500

    upgrade cluster

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "infra" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "bradfordwagner"
-  kubernetes_version  = "1.21.2"
+  kubernetes_version  = "1.21.9"
 
   network_profile {
     network_plugin    = "kubenet"
